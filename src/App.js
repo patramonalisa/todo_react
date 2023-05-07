7bb3f2b692ff052de90f9876ff6aa07947b0678b
patramonalisa <patramonalisa20@gmail.com>
2023-05-07 18:15:14 +0530

    added changes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import TodoList from "./components/TodoList"
+
+function App() {
+  return (
+    <div className="todo-app">
+      {/* <h1>hiii</h1> */}
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
